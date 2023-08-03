0aa441336e5508b953d47eab6e3138bad47cdeba
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-03 17:00:20 +0530

    simple todo

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,22 @@
 "use strict";
-const btn = document.getElementById("btn");
-const input = document.getElementById("todoInput"); // type assertion
-const form = document.querySelector("form"); //non null assertion operator  "!"
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoInput'); // type assertion
+const form = document.querySelector('form'); //non null assertion operator  "!"
+const list = document.getElementById('todolist');
 // // "?" null or not
 // btn?.addEventListener('click', () => {
 //   alert(input.value)
 // })
-form.addEventListener("submit", handleSubmit);
+form.addEventListener('submit', handleSubmit);
+//we use SubmitEvent as the type for the event
 function handleSubmit(event) {
-  event.preventDefault(); // Prevents the default form submission behavior
-  console.log("Submitted!");
+    event.preventDefault();
+    const newTodoText = input.value;
+    const newList = document.createElement("LI");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newList.append(newTodoText);
+    newList.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newList);
 }
 console.log(input);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 const btn = document.getElementById('btn')! as HTMLInputElement;
 const input = document.getElementById('todoInput')! as HTMLInputElement;   // type assertion
-
 const form = document.querySelector('form')!; //non null assertion operator  "!"
+
+const list = document.getElementById('todolist')
 
 // // "?" null or not
 // btn?.addEventListener('click', () => {
@@ -14,7 +15,19 @@ form.addEventListener('submit', handleSubmit);
 //we use SubmitEvent as the type for the event
 function handleSubmit(event: SubmitEvent) {
   event.preventDefault();
-  console.log("Submitted!");
+  const newTodoText = input.value;
+  const newList = document.createElement("LI");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  newList.append(newTodoText);
+  newList.append(checkbox);
+  list?.append(newList);
 }
 
 console.log(input)
+
+
+
+
+
+
