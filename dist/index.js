6c2409846c3fa8666d9d3a059505e7802029e7a1
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-03 18:02:41 +0530

    fix reload

--- dist/index.js
+++ dist/index.js
@@ -5,30 +5,48 @@ const form = document.querySelector('form'); //non null assertion operator  "!"
 const list = document.getElementById('todolist');
 const todoList = readTodos();
 form.addEventListener('submit', handleSubmit);
-//we use SubmitEvent as the type for the event
 function handleSubmit(event) {
     event.preventDefault();
     const newTodo = {
         text: input.value,
-        complete: false
+        complete: false,
     };
     createTodo(newTodo);
     todoList.push(newTodo);
-    localStorage.setItem("todos", JSON.stringify(todoList));
+    saveTodos();
     input.value = '';
 }
 function createTodo(todo) {
-    const newList = document.createElement("LI");
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
+    const newList = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.complete;
+    checkbox.addEventListener('change', () => {
+        todo.complete = checkbox.checked;
+        saveTodos();
+    });
     newList.append(todo.text);
     newList.append(checkbox);
     list === null || list === void 0 ? void 0 : list.append(newList);
 }
 function readTodos() {
-    const todosJSON = localStorage.getItem('todos');
-    if (todosJSON == null)
+    try {
+        const todosJSON = localStorage.getItem('todos');
+        if (todosJSON == null)
+            return [];
+        return JSON.parse(todosJSON);
+    }
+    catch (error) {
+        console.error('Error reading data from localStorage:', error);
         return [];
-    return JSON.parse(todosJSON);
+    }
 }
-console.log(input);
+function saveTodos() {
+    try {
+        localStorage.setItem('todos', JSON.stringify(todoList));
+        console.log('Data saved successfully.');
+    }
+    catch (error) {
+        console.error('Error saving data to localStorage:', error);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -9,54 +9,59 @@ const list = document.getElementById('todolist')
 //   alert(input.value)
 
 // })
-
 interface Todo {
-  text: string,
-  complete: boolean
+  text: string;
+  complete: boolean;
 }
 
 const todoList: Todo[] = readTodos();
 
 form.addEventListener('submit', handleSubmit);
 
-//we use SubmitEvent as the type for the event
-function handleSubmit(event: SubmitEvent) {
+function handleSubmit(event: Event) {
   event.preventDefault();
   const newTodo: Todo = {
     text: input.value,
-    complete: false
-  }
+    complete: false,
+  };
 
   createTodo(newTodo);
   todoList.push(newTodo);
 
-  localStorage.setItem("todos", JSON.stringify(todoList));
-  input.value = ''
-
+  saveTodos();
+  input.value = '';
 }
 
-
 function createTodo(todo: Todo) {
-
-  const newList = document.createElement("LI");
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
+  const newList = document.createElement('li');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = todo.complete;
+  checkbox.addEventListener('change', () => {
+    todo.complete = checkbox.checked;
+    saveTodos();
+  });
   newList.append(todo.text);
   newList.append(checkbox);
   list?.append(newList);
 }
 
-
 function readTodos(): Todo[] {
-  const todosJSON = localStorage.getItem('todos')
-  if (todosJSON == null) return [];
-  return JSON.parse(todosJSON)
+  try {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON == null) return [];
+    return JSON.parse(todosJSON);
+  } catch (error) {
+    console.error('Error reading data from localStorage:', error);
+    return [];
+  }
 }
 
-console.log(input)
-
-
-
-
-
-
+function saveTodos(): void {
+  try {
+    localStorage.setItem('todos', JSON.stringify(todoList));
+    console.log('Data saved successfully.');
+  } catch (error) {
+    console.error('Error saving data to localStorage:', error);
+  }
+}
