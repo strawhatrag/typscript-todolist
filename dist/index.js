b5b1b76e2747e88988c16d29da5b738fe636b90d
strawhatrag <sreeragbharathan791@gmail.com>
2023-08-03 12:03:04 +0530

    add form and event listeners

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-const btn = document.getElementById('btn');
-console.log(btn);
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoInput"); // type assertion
+const form = document.querySelector("form"); //non null assertion operator  "!"
+// // "?" null or not
+// btn?.addEventListener('click', () => {
+//   alert(input.value)
+// })
+form.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault(); // Prevents the default form submission behavior
+  console.log("Submitted!");
+}
+console.log(input);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
-const btn = document.getElementById('btn')
+const btn = document.getElementById('btn')! as HTMLInputElement;
+const input = document.getElementById('todoInput')! as HTMLInputElement;   // type assertion
 
+const form = document.querySelector('form')!; //non null assertion operator  "!"
 
-console.log(btn)
+// // "?" null or not
+// btn?.addEventListener('click', () => {
+//   alert(input.value)
+
+// })
+
+form.addEventListener('submit', handleSubmit);
+
+//we use SubmitEvent as the type for the event
+function handleSubmit(event: SubmitEvent) {
+  event.preventDefault();
+  console.log("Submitted!");
+}
+
+console.log(input)
